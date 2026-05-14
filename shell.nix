26e9c79fd1778f7721c40e0b760666f0d5434d5a
Vanger <VangerSpecial@protonmail.com>
2026-05-14 13:03:53 +0300

    fix: use absolute paths in shell.nix for environment variables

--- shell.nix
+++ shell.nix
@@ -1,5 +1,7 @@
 let
   pkgs = import <nixpkgs> {};
+  currentDir = builtins.toString ./.;
+  testDir = "${currentDir}/test/test-res";
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
@@ -11,10 +13,10 @@ pkgs.mkShell {
   ];
   SSHCB_PORT = "2456";
   SSHCB_SERVER_INTERFACE_IP = "127.0.0.1";
-  SSHCB_KNOWNHOST = "test-res/known_hosts";
-  SSHCB_AUTHORIZED_KEYS = "test-res/known_users";
-  SSHCB_SERVER_PUBKEY_FILE = "test-res/server.pub";
-  SSHCB_SERVER_PRIVKEY_FILE = "test-res/server";
-  SSHCB_CLIENT_PUBKEY_FILE = "test-res/client.pub";
-  SSHCB_CLIENT_PRIVKEY_FILE = "test-res/client";
+  SSHCB_KNOWNHOST = "${testDir}/known_hosts";
+  SSHCB_AUTHORIZED_KEYS = "${testDir}/known_users";
+  SSHCB_SERVER_PUBKEY_FILE = "${testDir}/server.pub";
+  SSHCB_SERVER_PRIVKEY_FILE = "${testDir}/server";
+  SSHCB_CLIENT_PUBKEY_FILE = "${testDir}/client.pub";
+  SSHCB_CLIENT_PRIVKEY_FILE = "${testDir}/client";
 }
